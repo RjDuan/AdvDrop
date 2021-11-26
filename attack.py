cee25322149864b950b64d420a3a2ae5455e3908
HashmatShadab <hashmat.shadab.malik@gmail.com>
2021-11-26 17:28:14 +0400

    training loop changes

--- attack.py
+++ attack.py
@@ -13,27 +13,13 @@ from torchvision import models
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 from torchattacks.attack import Attack
-
+import argparse
 from utils import *
 from compression import *
 from decompression import *
 from PIL import ImageFile
 
-ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-attack_methods = {"PGD": [torchattacks.PGD, "l_inf"],
-                "PGDL2": [torchattacks.PGDL2, "l2"],
-                "FGSM": [torchattacks.FGSM, "l_inf"],
-                "BIM": [torchattacks.BIM, "l_inf"],
-                 "CW" : [torchattacks.CW, "l2"],
-                 "Dfool": [torchattacks.DeepFool, "l2"]
-                }
-
-"""
-We set epsilon = 4 for attacks on l∞ setting, set epsilon = 0.06 for attacks
-on l2 setting that are common used in previous defense methods.
-We set quantization table q with 100 for AdvDrop.
-"""
 class Normalize(nn.Module):
     def __init__(self, mean, std):
         super(Normalize, self).__init__()
@@ -46,15 +32,11 @@ class Normalize(nn.Module):
         mean = self.mean.reshape(1, 3, 1, 1)
         std = self.std.reshape(1, 3, 1, 1)
         return (input - mean) / std
-
-
 def save_img(img, img_name, save_dir):
     create_dir(save_dir)
     img_path = os.path.join(save_dir, img_name)
     img_pil = Image.fromarray(img.astype(np.uint8))
     img_pil.save(img_path)
-
-
 def pred_label_and_confidence(model, input_batch, labels_to_class):
     input_batch = input_batch.cuda()
     with torch.no_grad():
@@ -69,28 +51,36 @@ def pred_label_and_confidence(model, input_batch, labels_to_class):
         pred_conf = str(round(percentage[i][index[i]].item(), 2))
         pred_list.append([pred_class, pred_conf])
     return pred_list
-
-
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(), ])
-
-
 norm_layer = Normalize(mean=[0.485, 0.456, 0.406],
                        std=[0.229, 0.224, 0.225])
-
 resnet_model = nn.Sequential(
     norm_layer,
     models.resnet50(pretrained=True)
 ).to(device)
 resnet_model = resnet_model.eval()
 
-# Uncomment if you want save results
-# save_dir = "./results"
-# create_dir(save_dir)
+attack_methods = {"PGD": [torchattacks.PGD, "l_inf"],
+                "PGDL2": [torchattacks.PGDL2, "l2"],
+                "FGSM": [torchattacks.FGSM, "l_inf"],
+                "BIM": [torchattacks.BIM, "l_inf"],
+                 "CW" : [torchattacks.CW, "l2"],
+                 "Dfool": [torchattacks.DeepFool, "l2"]
+                }
+
+"""
+We set epsilon = 4 for attacks on l∞ setting, set epsilon = 0.06 for attacks
+on l2 setting that are common used in previous defense methods.
+We set quantization table q with 100 for AdvDrop.
+"""
+
+
+
+
 batch_size = 20
 tar_cnt = 1000
 cur_cnt = 0
@@ -104,15 +94,45 @@ normal_loader = get_data(data_dir="./test-data", transform=transform, batch_size
 save_dir = "./results"
 create_dir(save_dir)
 
+"""
+eps: maximum perturbation
+alpha: step_size
+overshoot: parameter for enhancing the noise 
+c : in the paper, parameter for box-constraint. (Default: 1e-4) 
+kappa :  kappa (also written as ‘confidence’) in the paper. (Default: 0) 
+lr (float) : learning rate of the Adam optimizer. (Default: 0.01)
+
+>>>attack = torchattacks.DeepFool(model, steps=50, overshoot=0.02)
+>>>adv_images = attack(images, labels)
+
+>>> attack = torchattacks.PGDL2(model, eps=1.0, alpha=0.2, steps=40, random_start=True)
+>>> adv_images = attack(images, labels)
+
+>>> attack = torchattacks.PGD(model, eps=8/255, alpha=1/255, steps=40, random_start=True)
+>>> adv_images = attack(images, labels)
 
 
-#normal_iter = iter(normal_loader)
+>>> attack = torchattacks.BIM(model, eps=4/255, alpha=1/255, steps=0)
+>>> adv_images = attack(images, labels)
+
+>>> attack = torchattacks.CW(model, c=1e-4, kappa=0, steps=1000, lr=0.01)
+>>> adv_images = attack(images, labels)
+
+>>> attack = torchattacks.FGSM(model, eps=0.007)
+>>> adv_images = attack(images, labels)
+default: untargetted attacks
+"""
+method = "PGD"
+normal_iter = iter(normal_loader)
 for i in range(tar_cnt // batch_size):
     print("Iter: ", i)
     images, labels = normal_iter.next()
-    attack = torchattacks.PGD(resnet_model, eps=eps, alpha=1 / 255, steps=steps,
+    attack_method, bound = attack_methods[method][0], attack_methods[method][1]
+    attack = attack_method(resnet_model, eps=eps, alpha=1 / 255, steps=steps,
                               random_start=True)
     at_images = attack(images, labels)
+    outputs = resnet_model(at_images)
+    _, pre = torch.max(outputs.data, 1)
     # Uncomment following codes if you wang to save the adv imgs
     at_images_np = at_images.detach().cpu().numpy()
     adv_img = at_images_np[0]
@@ -122,6 +142,5 @@ for i in range(tar_cnt // batch_size):
     save_img(adv_img, img_name, adv_dir)
     labels = torch.from_numpy(np.random.randint(0, 1000, size=batch_size))
 
-    images = images * 255.0
 
 
